0f966ae4279730e8144f6cefd434368e7f4b7add
dmitriy.balashov <reaver_std@mail.ru>
2023-06-17 16:22:49 +0500

    add work with crypto

--- main.py
+++ main.py
@@ -7,31 +7,44 @@ from os import getenv
 CURRENCY_RATES_FILE = "currency_rates.json"
 
 TELEGRAM_API_KEY = getenv('PAVEL_TELEGRAM_API_KEY')
-API_KEY = getenv('APILAYER_API_KEY')
+APILAYER_API_KEY = getenv('APILAYER_API_KEY')
+FINNHUB_API_KEY = getenv('FINNHUB_API_KEY')
 
 bot = telebot.TeleBot(TELEGRAM_API_KEY)
 
 
-def get_currency_rate(currency: str):
+def get_currency_rate(currency: str) -> dict:
     """Получает курс валюты от API и возвращает его в виде float"""
 
     url = f"https://api.apilayer.com/exchangerates_data/latest?base={currency}"
-    response = requests.get(url, headers={'apikey': API_KEY})
+    response = requests.get(url, headers={'apikey': APILAYER_API_KEY})
     response_data = json.loads(response.text)
     return response_data
 
 
-def save_to_json(data_to_save: dict):
-    """Сохраняет данные в JSON-файл"""
-    with open(CURRENCY_RATES_FILE) as json_file:
-        data = json.load(json_file)
-    if round(data['rates']['RUB'], 2) == round(data_to_save['rates']['RUB'], 2):
-        return False
-    else:
-        with open(CURRENCY_RATES_FILE, "w") as json_file:
-            json.dump(data_to_save, json_file)
-    return True
+def get_crypro_price(crypto: str) -> str:
+    params = {
+        'symbol': f'BINANCE:{crypto}',
+        'resolution': 1,
+        'token': FINNHUB_API_KEY
+    }
+    url = "https://finnhub.io/api/v1/crypto/candle"
+    response = requests.get(url, params=params)
+    data_candles = json.loads(response.text)
+    return data_candles['c'][-1]
 
+
+# def save_to_json(data_to_save: dict):
+#     """Сохраняет данные в JSON-файл"""
+#     with open(CURRENCY_RATES_FILE) as json_file:
+#         data = json.load(json_file)
+#     if round(data['rates']['RUB'], 2) == round(data_to_save['rates']['RUB'], 2):
+#         return False
+#     else:
+#         with open(CURRENCY_RATES_FILE, "w") as json_file:
+#             json.dump(data_to_save, json_file)
+#     return True
+#
 
 @bot.message_handler(commands=['start'])
 def start(message) -> None:
@@ -58,15 +71,14 @@ def get_text_messages(message) -> None:
     if message.text == '👋 Поздороваться с Павлом' or message.text == 'Назад':
         markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
         btn1 = telebot.types.KeyboardButton('Валюты')
-        btn2 = telebot.types.KeyboardButton('Акции')
+        btn2 = telebot.types.KeyboardButton('Криптовалюты')
         btn3 = telebot.types.KeyboardButton('Youtube')
         markup.add(btn1, btn2, btn3)
         bot.send_message(message.from_user.id, '<b>Выберите интересующий вас раздел</b>',
                          reply_markup=markup, parse_mode='html')
-    elif message.text in ['Валюты', 'Акции', 'Youtube']:
+    elif message.text in ['Валюты', 'Криптовалюты', 'Youtube']:
         commands(message)
     elif message.text == 'Доллар':
-        print("работает")
         data = get_currency_rate("USD")
         bot.send_message(message.from_user.id, f"За 1 доллар сейчас дают {round(data['rates']['RUB'], 2)}р.")
     elif message.text == 'Евро':
@@ -75,8 +87,15 @@ def get_text_messages(message) -> None:
     elif message.text == 'Фунт стерлингов':
         data = get_currency_rate("GBP")
         bot.send_message(message.from_user.id, f"За 1 фунт стерлингов сейчас дают {round(data['rates']['RUB'], 2)}р.")
-    elif message.text == '':
-        pass
+    elif message.text == 'Bitcoin':
+        data = get_crypro_price("BTCUSDT")
+        bot.send_message(message.from_user.id, f"За 1 BTC сейчас дают {data} USDT")
+    elif message.text == 'Etherium':
+        data = get_crypro_price("ETHUSDT")
+        bot.send_message(message.from_user.id, f"За 1 ETH сейчас дают {data} USDT")
+    elif message.text == 'Litecoin':
+        data = get_crypro_price("LTCUSDT")
+        bot.send_message(message.from_user.id, f"За 1 LTC сейчас дают {data} USDT")
 
 
 def commands(message):
@@ -89,35 +108,17 @@ def commands(message):
         markup.add(btn1, btn2, btn3, btn4)
         bot.send_message(message.from_user.id, '<b>Выберите валюту</b>',
                          reply_markup=markup, parse_mode='html')
-    elif message.text == "Акции":
-        bot.send_message(message.from_user.id, 'Функция для работы с акциями')
+    elif message.text == "Криптовалюты":
+        markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+        btn1 = telebot.types.KeyboardButton('Bitcoin')
+        btn2 = telebot.types.KeyboardButton('Etherium')
+        btn3 = telebot.types.KeyboardButton('Litecoin')
+        btn4 = telebot.types.KeyboardButton('Назад')
+        markup.add(btn1, btn2, btn3, btn4)
+        bot.send_message(message.from_user.id, '<b>Выберите криптовалюту</b>',
+                         reply_markup=markup, parse_mode='html')
     elif message.text == "Youtube":
         bot.send_message(message.from_user.id, 'Функция для работы с Youtube')
-
-
-def currency(message):
-
-
-    # elif message.text == 'Доллар':
-    #     data = get_currency_rate("USD")
-    #     if save_to_json(data):
-    #         bot.send_message(message.from_user.id, f"За 1 доллар сейчас дают {round(data['rates']['RUB'], 2)}р.",
-    #                          parse_mode='Markdown')
-    #     else:
-    #         bot.send_message(message.from_user.id, f"Курс не изменился")
-    #
-    # elif message.text == 'Евро':
-    #     data = get_currency_rate("EUR")
-    #     if save_to_json(data):
-    #         bot.send_message(message.from_user.id, f"За 1 евро сейчас дают {round(data['rates']['RUB'], 2)}р.",
-    #                          parse_mode='Markdown')
-    #     else:
-    #         bot.send_message(message.from_user.id, f"Курс не изменился")
-    #
-    # elif message.text == "Выход":
-    #     bot.send_message(message.from_user.id, "Разваливаюсь... Начните писать что-нибудь, чтобы добить меня",
-    #                      parse_mode='Markdown')
-    #     bot.stop_bot()
 
 
 bot.polling(none_stop=True)  # обязательная для работы бота часть
